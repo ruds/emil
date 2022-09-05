c53d79dbe673cc2125b94ac30f5303f8ec9dadd2
Matt Rudary <matt@rudary.com>
2022-09-05 16:56:40 -0400

    Add tests for Source.

--- src/source.cc
+++ src/source.cc
@@ -1,18 +1,29 @@
 #include "emil/source.h"
 
+#include <any>
 #include <deque>
 #include <istream>
 #include <iterator>
 #include <memory>
 #include <optional>
+#include <string>
+#include <utility>
 
 namespace emil {
+
+using std::cbegin;
+using std::cend;
 
 namespace {
 template <typename It>
 class IteratorSource : public Source {
  public:
-  IteratorSource(It begin, It end) : in_(begin), end_(end) {}
+  IteratorSource(It begin, It end) : container_(), in_(begin), end_(end) {}
+  template <typename T>
+  explicit IteratorSource(T&& container)
+      : container_(std::forward<T>(container)),
+        in_(cbegin(any_cast<T&>(container_))),
+        end_(cend(any_cast<T&>(container_))) {}
   ~IteratorSource() override = default;
 
   char32_t advance() override {
@@ -39,6 +50,7 @@ class IteratorSource : public Source {
   }
 
  private:
+  std::any container_;
   It in_;
   It end_;
   std::deque<char32_t> lookahead_buffer_;
@@ -49,6 +61,11 @@ std::unique_ptr<Source> make_source(std::basic_istream<char32_t>& in) {
   return std::make_unique<IteratorSource<std::istreambuf_iterator<char32_t>>>(
       std::istreambuf_iterator<char32_t>{in},
       std::istreambuf_iterator<char32_t>{});
+}
+
+std::unique_ptr<Source> make_source(std::u32string in) {
+  return std::make_unique<IteratorSource<std::u32string::const_iterator>>(
+      std::move(in));
 }
 
 }  // namespace emil
---
+++ test/source_test.cc
@@ -0,0 +1,102 @@
+#include "emil/source.h"
+
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <sstream>
+
+namespace emil::testing {
+namespace {
+
+struct use_stream_t {};
+
+using iss32 = std::basic_istringstream<char32_t>;
+
+struct TestParam {
+  std::shared_ptr<iss32> stream;
+  std::shared_ptr<Source> source;
+
+  explicit TestParam(use_stream_t)
+      : stream(std::make_shared<iss32>(U"1234")),
+        source(make_source(*stream)) {}
+  TestParam() : source(make_source(U"1234")) {}
+};
+
+[[maybe_unused]] void PrintTo(const TestParam& t, std::ostream* out) {
+  *out << (t.stream ? "stream" : "string");
+}
+
+class SourceTest : public ::testing::TestWithParam<TestParam> {};
+
+INSTANTIATE_TEST_SUITE_P(Sources, SourceTest,
+                         ::testing::Values(TestParam{},
+                                           TestParam{use_stream_t{}}));
+
+TEST_P(SourceTest, Advances) {
+  Source& source = *GetParam().source;
+  ASSERT_FALSE(source.at_end());
+
+  EXPECT_EQ(source.advance(), '1');
+  ASSERT_FALSE(source.at_end());
+
+  EXPECT_EQ(source.advance(), '2');
+  ASSERT_FALSE(source.at_end());
+
+  EXPECT_EQ(source.advance(), '3');
+  ASSERT_FALSE(source.at_end());
+
+  EXPECT_EQ(source.advance(), '4');
+  EXPECT_TRUE(source.at_end());
+}
+
+TEST_P(SourceTest, PeeksOne) {
+  Source& source = *GetParam().source;
+  ASSERT_FALSE(source.at_end());
+
+  EXPECT_EQ(source.peek(), '1');
+  EXPECT_EQ(source.advance(), '1');
+  ASSERT_FALSE(source.at_end());
+
+  EXPECT_EQ(source.peek(), '2');
+  EXPECT_EQ(source.advance(), '2');
+  ASSERT_FALSE(source.at_end());
+
+  EXPECT_EQ(source.peek(), '3');
+  EXPECT_EQ(source.advance(), '3');
+  ASSERT_FALSE(source.at_end());
+
+  EXPECT_EQ(source.peek(), '4');
+  EXPECT_EQ(source.advance(), '4');
+  EXPECT_TRUE(source.at_end());
+
+  EXPECT_FALSE(source.peek());
+}
+
+TEST_P(SourceTest, PeeksAll) {
+  Source& source = *GetParam().source;
+  ASSERT_FALSE(source.at_end());
+
+  EXPECT_EQ(source.peek(3), '4');
+  EXPECT_FALSE(source.peek(4));
+
+  EXPECT_EQ(source.peek(), '1');
+  EXPECT_EQ(source.advance(), '1');
+  ASSERT_FALSE(source.at_end());
+
+  EXPECT_EQ(source.peek(), '2');
+  EXPECT_EQ(source.advance(), '2');
+  ASSERT_FALSE(source.at_end());
+
+  EXPECT_EQ(source.peek(), '3');
+  EXPECT_EQ(source.advance(), '3');
+  ASSERT_FALSE(source.at_end());
+
+  EXPECT_EQ(source.peek(), '4');
+  EXPECT_EQ(source.advance(), '4');
+  EXPECT_TRUE(source.at_end());
+
+  EXPECT_FALSE(source.peek());
+}
+
+}  // namespace
+}  // namespace emil::testing
