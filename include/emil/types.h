ed98279986ef9113ef44f8709d95ea6656330ef8
Matt Rudary <matt@rudary.com>
2022-11-02 11:01:05 -0400

    Add a hierarchy of semantic objects for typing.

---
+++ include/emil/types.h
@@ -0,0 +1,338 @@
+// Copyright 2022 Matt Rudary
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <cstddef>
+#include <optional>
+#include <string_view>
+#include <type_traits>
+
+#include "emil/collections.h"
+#include "emil/gc.h"
+#include "emil/string.h"
+
+/**
+ * @file types.h
+ *
+ * @brief Provides types and functionality used when doing static type
+ * analysis on emil programs.
+ *
+ * Objects in the `TypeObj` hierarchy are managed by the runtime
+ * since, in general, they will stick around for static type analysis
+ * of further top-level declarations. This hierarchy closely follows
+ * the static semantic objects described in Chapter 4 of the
+ * Definition of Standard ML (Revised).
+ */
+
+namespace emil {
+
+namespace typing {
+
+class TypeObj;
+
+template <typename T>
+concept TypeObjType = std::is_base_of<TypeObj, T>::value;
+
+using StringSet = collections::SetPtr<ManagedString>;
+
+template <TypeObjType T>
+using StringMap = collections::MapPtr<ManagedString, T>;
+
+/**
+ * @brief Objects that describe types and collections of types.
+ *
+ * These are the same as the "semantic objects" described in the Definition.
+ */
+class TypeObj : public Managed {
+ public:
+  const StringSet& free_variables() const { return free_variables_; }
+
+ protected:
+  TypeObj(StringSet free_variables);
+  ~TypeObj() override;
+
+ private:
+  const StringSet free_variables_;
+
+  virtual void visit_additional_subobjects(const ManagedVisitor& visitor) = 0;
+
+  void visit_subobjects(const ManagedVisitor& visitor) final;
+};
+
+class Type : public TypeObj {
+ protected:
+  using TypeObj::TypeObj;
+};
+using TypePtr = managed_ptr<Type>;
+
+/** A type variable. */
+class TypeVar : public Type {
+ public:
+  explicit TypeVar(std::u8string_view name);
+  explicit TypeVar(StringPtr name);
+
+  std::u8string_view name() const { return *name_; }
+  StringPtr name_ptr() const { return name_; }
+
+  void visit_additional_subobjects(const ManagedVisitor&) override;
+  std::size_t managed_size() const noexcept override { return sizeof(TypeVar); }
+
+ private:
+  StringPtr name_;
+};
+
+/** A globally unique name assigned to a constructed type. */
+class TypeName : public Type {
+ public:
+  // TODO: add unique id
+  TypeName(std::u8string_view name, std::size_t arity);
+  TypeName(StringPtr name, std::size_t arity);
+
+  std::u8string_view name() const { return *name_; }
+  StringPtr name_ptr() const { return name_; }
+  std::size_t arity() const { return arity_; }
+
+ private:
+  StringPtr name_;
+  const std::size_t arity_;
+
+  void visit_additional_subobjects(const ManagedVisitor&) override;
+  std::size_t managed_size() const noexcept override {
+    return sizeof(TypeName);
+  }
+};
+
+/** A mapping from labels to types. */
+class RecordType : public Type {
+ public:
+  explicit RecordType(StringMap<Type> rows);
+
+  StringMap<Type> rows() const { return rows_; }
+
+ private:
+  StringMap<Type> rows_;
+
+  void visit_additional_subobjects(const ManagedVisitor& visitor) override;
+  std::size_t managed_size() const noexcept override {
+    return sizeof(RecordType);
+  }
+};
+
+/** The type of a function object. */
+class FunctionType : public Type {
+ public:
+  FunctionType(TypePtr param, TypePtr result);
+
+  TypePtr param() const { return param_; }
+  TypePtr result() const { return result_; }
+
+ private:
+  const TypePtr param_;
+  const TypePtr result_;
+
+  void visit_additional_subobjects(const ManagedVisitor& visitor) override;
+  std::size_t managed_size() const noexcept override {
+    return sizeof(FunctionType);
+  }
+};
+
+/** A constructed type: a type constructor and its type parameters. */
+class ConstructedType : public Type {
+ public:
+  ConstructedType(managed_ptr<TypeName> name,
+                  collections::ArrayPtr<Type> types);
+
+  std::u8string_view name_str() const { return name_->name(); }
+  managed_ptr<TypeName> name() const { return name_; }
+  const collections::ArrayPtr<Type>& types() const { return types_; }
+
+ private:
+  managed_ptr<TypeName> name_;
+  const collections::ArrayPtr<Type> types_;
+
+  void visit_additional_subobjects(const ManagedVisitor& visitor) override;
+  std::size_t managed_size() const noexcept override {
+    return sizeof(ConstructedType);
+  }
+};
+
+/** All bound identifiers are associated with one of these statuses. */
+enum class IdStatus {
+  Exception,
+  Variable,
+  Constructor,
+};
+
+/**
+ * @brief A mapping from type variables to a type.
+ *
+ * A `TypeFunction` binds its variables (which must be distinct). That
+ * is, the free variables of a type function are the free variables of
+ * the resulting type expression *excluding the bound variables*.
+ */
+class TypeFunction : public TypeObj {
+ public:
+  TypeFunction(TypePtr t, collections::ArrayPtr<TypeVar> bound);
+
+  TypePtr t() const { return t_; }
+  const collections::ArrayPtr<TypeVar>& bound() const { return bound_; }
+
+ private:
+  const TypePtr t_;
+  const collections::ArrayPtr<TypeVar> bound_;
+
+  void visit_additional_subobjects(const ManagedVisitor& visitor) override;
+  std::size_t managed_size() const noexcept override {
+    return sizeof(TypeFunction);
+  }
+};
+
+/**
+ * @brief A particular generalization of a type.
+ *
+ * A type scheme is a generalization of a type using a universal
+ * quantifier: ∀ɑ1,ɑ2,...,ɑk τ. The free variables of a type scheme
+ * are the free variables of the type excluding the quantified
+ * variables.
+ */
+class TypeScheme : public TypeObj {
+ public:
+  TypeScheme(TypePtr t, collections::ArrayPtr<TypeVar> bound);
+
+  TypePtr t() const { return t_; }
+  const collections::ArrayPtr<TypeVar>& bound() const { return bound_; }
+
+ private:
+  const TypePtr t_;
+  const collections::ArrayPtr<TypeVar> bound_;
+
+  void visit_additional_subobjects(const ManagedVisitor& visitor) override;
+  std::size_t managed_size() const noexcept override {
+    return sizeof(TypeScheme);
+  }
+};
+
+class ValEnv;
+
+/**
+ * @brief The type structure that a type constructor is bound to.
+ *
+ * A type structure is only well-formed if `env` is empty or `fn` is
+ * (∀ɑ1,ɑ2,...,ɑk t) for some TypeName t.
+ */
+class TypeStructure : public TypeObj {
+ public:
+  TypeStructure(managed_ptr<TypeFunction> fn, managed_ptr<ValEnv> env);
+
+  const managed_ptr<TypeFunction>& fn() const { return fn_; }
+  const managed_ptr<ValEnv>& env() const { return env_; }
+
+ private:
+  const managed_ptr<TypeFunction> fn_;
+  const managed_ptr<ValEnv> env_;
+
+  void visit_additional_subobjects(const ManagedVisitor& visitor) override;
+  std::size_t managed_size() const noexcept override {
+    return sizeof(TypeStructure);
+  }
+};
+
+/** The mapping between type constructor names and type structures in an
+ * environment. */
+class TypeEnv : public TypeObj {
+ public:
+  explicit TypeEnv(StringMap<TypeStructure> env);
+
+  std::optional<managed_ptr<TypeStructure>> get(
+      const std::u8string_view& key) const;
+
+ private:
+  StringMap<TypeStructure> env_;
+
+  void visit_additional_subobjects(const ManagedVisitor& visitor) override;
+  std::size_t managed_size() const noexcept override { return sizeof(TypeEnv); }
+};
+
+/** The type scheme and identifier status associated with a value variable in an
+ * environment. */
+class ValueBinding : public TypeObj {
+  ValueBinding(managed_ptr<TypeScheme> scheme, IdStatus status);
+
+  managed_ptr<TypeScheme> scheme() const { return scheme_; }
+  IdStatus status() const { return status_; }
+
+ private:
+  const managed_ptr<TypeScheme> scheme_;
+  const IdStatus status_;
+
+  void visit_additional_subobjects(const ManagedVisitor& visitor) override;
+  std::size_t managed_size() const noexcept override {
+    return sizeof(ValueBinding);
+  }
+};
+
+/** The mapping from value variable identifiers to their value bindings. */
+class ValEnv : public TypeObj {
+ public:
+  explicit ValEnv(StringMap<ValueBinding> env);
+
+  std::optional<managed_ptr<ValueBinding>> get(
+      const std::u8string_view& key) const;
+
+ private:
+  StringMap<ValueBinding> env_;
+
+  void visit_additional_subobjects(const ManagedVisitor& visitor) override;
+  std::size_t managed_size() const noexcept override { return sizeof(ValEnv); }
+};
+
+/** The type environment of a program. */
+class Env : public TypeObj {
+ public:
+  Env(managed_ptr<TypeEnv> type_env, managed_ptr<ValEnv> val_env);
+
+  managed_ptr<TypeEnv> type_env() const { return type_env_; }
+  managed_ptr<ValEnv> val_env() const { return val_env_; }
+
+ private:
+  const managed_ptr<TypeEnv> type_env_;
+  const managed_ptr<ValEnv> val_env_;
+
+  void visit_additional_subobjects(const ManagedVisitor& visitor) override;
+  std::size_t managed_size() const noexcept override { return sizeof(Env); }
+};
+
+/** The type context of a program. */
+class Context : public TypeObj {
+ public:
+  Context(collections::SetPtr<TypeName> type_names, StringSet type_variables,
+          managed_ptr<Env> env);
+
+  const collections::SetPtr<TypeName>& type_names() const { return names_; }
+  const StringSet& type_variables() const { return vars_; }
+  managed_ptr<Env> env() const { return env_; }
+
+ private:
+  const collections::SetPtr<TypeName> names_;
+  const StringSet vars_;
+  const managed_ptr<Env> env_;
+
+  void visit_additional_subobjects(const ManagedVisitor& visitor) override;
+  std::size_t managed_size() const noexcept override { return sizeof(Context); }
+};
+
+}  // namespace typing
+
+}  // namespace emil
---
+++ src/types.cc
@@ -0,0 +1,219 @@
+// Copyright 2022 Matt Rudary
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "emil/types.h"
+
+#include <cassert>
+#include <initializer_list>
+#include <utility>
+
+#include "emil/collections.h"
+#include "emil/string.h"
+#include "emil/tree.h"
+
+namespace emil {
+
+namespace typing {
+
+namespace {
+
+StringSet string_set(std::initializer_list<StringPtr> strings = {}) {
+  return collections::managed_set<ManagedString>(strings);
+}
+
+template <typename T>
+concept TypeObjWithNamePtr = requires(const T& t) {
+  {StringPtr{t.name_ptr()}};  // NOLINT(readability/braces)
+};                            // NOLINT(readability/braces)
+
+template <TypeObjWithNamePtr T>
+StringSet to_distinct_string_set(const collections::ArrayPtr<T>& vars) {
+  auto s = string_set();
+  for (const auto& v : *vars) {
+    auto r = s->insert(v->name_ptr());
+    assert(r.second);
+    s = std::move(r.first);
+  }
+  return s;
+}
+
+// requires hold
+template <TypeObjType T>
+StringSet merge_free_variables(const StringMap<T>& m) {
+  StringSet s = string_set();
+  for (const auto& entry : *m) {
+    s = std::move(s) | entry.second->free_variables();
+  }
+  return s;
+}
+
+// requires hold
+StringSet merge_free_variables(const collections::ArrayPtr<Type>& types) {
+  StringSet s = string_set();
+  for (const auto& entry : *types) {
+    s = std::move(s) | entry->free_variables();
+  }
+  return s;
+}
+
+}  // namespace
+
+TypeObj::TypeObj(StringSet free_variables)
+    : free_variables_(std::move(free_variables)) {}
+
+TypeObj::~TypeObj() = default;
+
+void TypeObj::visit_subobjects(const ManagedVisitor& visitor) {
+  free_variables_.accept(visitor);
+  visit_additional_subobjects(visitor);
+}
+
+TypeVar::TypeVar(std::u8string_view name) : TypeVar(make_string(name)) {}
+
+TypeVar::TypeVar(StringPtr name)
+    : Type(string_set({name})), name_(std::move(name)) {}
+
+void TypeVar::visit_additional_subobjects(const ManagedVisitor& visitor) {
+  name_.accept(visitor);
+}
+
+TypeName::TypeName(std::u8string_view name, std::size_t arity)
+    : TypeName(make_string(name), arity) {}
+
+TypeName::TypeName(StringPtr name, std::size_t arity)
+    : Type(string_set()), name_(std::move(name)), arity_(arity) {}
+
+void TypeName::visit_additional_subobjects(const ManagedVisitor& visitor) {
+  name_.accept(visitor);
+}
+
+RecordType::RecordType(StringMap<Type> rows)
+    : Type(merge_free_variables(rows)), rows_(std::move(rows)) {}
+
+void RecordType::visit_additional_subobjects(const ManagedVisitor& visitor) {
+  rows_.accept(visitor);
+}
+
+FunctionType::FunctionType(TypePtr param, TypePtr result)
+    : Type(param->free_variables() | result->free_variables()),
+      param_(std::move(param)),
+      result_(std::move(result)) {}
+
+void FunctionType::visit_additional_subobjects(const ManagedVisitor& visitor) {
+  param_.accept(visitor);
+  result_.accept(visitor);
+}
+
+ConstructedType::ConstructedType(managed_ptr<TypeName> name,
+                                 collections::ArrayPtr<Type> types)
+    : Type(merge_free_variables(types)),
+      name_(std::move(name)),
+      types_(std::move(types)) {
+  assert(types->size() == name->arity());
+}
+
+void ConstructedType::visit_additional_subobjects(
+    const ManagedVisitor& visitor) {
+  name_.accept(visitor);
+  types_.accept(visitor);
+}
+
+TypeFunction::TypeFunction(TypePtr t, collections::ArrayPtr<TypeVar> bound)
+    : TypeObj(t->free_variables() - to_distinct_string_set(bound)),
+      t_(std::move(t)),
+      bound_(std::move(bound)) {}
+
+void TypeFunction::visit_additional_subobjects(const ManagedVisitor& visitor) {
+  t_.accept(visitor);
+  bound_.accept(visitor);
+}
+
+TypeScheme::TypeScheme(TypePtr t, collections::ArrayPtr<TypeVar> bound)
+    : TypeObj(t->free_variables() - to_distinct_string_set(bound)),
+      t_(std::move(t)),
+      bound_(std::move(bound)) {}
+
+void TypeScheme::visit_additional_subobjects(const ManagedVisitor& visitor) {
+  t_.accept(visitor);
+  bound_.accept(visitor);
+}
+
+TypeStructure::TypeStructure(managed_ptr<TypeFunction> fn,
+                             managed_ptr<ValEnv> env)
+    : TypeObj(fn->free_variables() | env->free_variables()),
+      fn_(std::move(fn)),
+      env_(std::move(env)) {}
+
+void TypeStructure::visit_additional_subobjects(const ManagedVisitor& visitor) {
+  fn_.accept(visitor);
+  env_.accept(visitor);
+}
+
+TypeEnv::TypeEnv(StringMap<TypeStructure> env)
+    : TypeObj(merge_free_variables(env)), env_(std::move(env)) {}
+
+std::optional<managed_ptr<TypeStructure>> TypeEnv::get(
+    const std::u8string_view& key) const {
+  return env_->get(key);
+}
+
+void TypeEnv::visit_additional_subobjects(const ManagedVisitor& visitor) {
+  env_.accept(visitor);
+}
+
+ValueBinding::ValueBinding(managed_ptr<TypeScheme> scheme, IdStatus status)
+    : TypeObj(scheme->free_variables()),
+      scheme_(std::move(scheme)),
+      status_(status) {}
+
+void ValueBinding::visit_additional_subobjects(const ManagedVisitor& visitor) {
+  scheme_.accept(visitor);
+}
+
+ValEnv::ValEnv(StringMap<ValueBinding> env)
+    : TypeObj(merge_free_variables(env)), env_(std::move(env)) {}
+
+std::optional<managed_ptr<ValueBinding>> ValEnv::get(
+    const std::u8string_view& key) const {
+  return env_->get(key);
+}
+
+void ValEnv::visit_additional_subobjects(const ManagedVisitor& visitor) {
+  env_.accept(visitor);
+}
+
+Env::Env(managed_ptr<TypeEnv> type_env, managed_ptr<ValEnv> val_env)
+    : TypeObj(type_env->free_variables() | val_env->free_variables()),
+      type_env_(std::move(type_env)),
+      val_env_(std::move(val_env)) {}
+
+void Env::visit_additional_subobjects(const ManagedVisitor& visitor) {
+  type_env_.accept(visitor);
+  val_env_.accept(visitor);
+}
+
+Context::Context(collections::SetPtr<TypeName> type_names,
+                 StringSet type_variables, managed_ptr<Env> env)
+    : TypeObj(type_variables | env->free_variables()),
+      names_(std::move(type_names)),
+      vars_(std::move(type_variables)) {}
+
+void Context::visit_additional_subobjects(const ManagedVisitor& visitor) {
+  names_.accept(visitor);
+  vars_.accept(visitor);
+  env_.accept(visitor);
+}
+
+}  // namespace typing
+}  // namespace emil
