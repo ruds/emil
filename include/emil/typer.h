06ee3f3aa38470e0d99813ceca65e16ece75bb98
Matt Rudary <matt@rudary.com>
2023-06-02 15:22:38 -0400

    Elaborate on typer's public interface.

--- include/emil/typer.h
+++ include/emil/typer.h
@@ -14,43 +14,102 @@
 
 #pragma once
 
+#include <cstdint>
+#include <exception>
 #include <memory>
+#include <string>
 #include <utility>
 
-#include "emil/ast.h"
+#include "emil/ast.h"  // IWYU pragma: keep
 #include "emil/gc.h"
-#include "emil/typed_ast.h"
-#include "emil/types.h"  // IWYU pragma: keep
+#include "emil/token.h"
+#include "emil/typed_ast.h"  // IWYU pragma: keep
+#include "emil/types.h"
 
 namespace emil {
 
+/** Error thrown due to error in static elaboration. */
+class ElaborationError : public std::exception {
+ public:
+  ElaborationError(std::string msg, const Location& location);
+
+  virtual const char* what() const noexcept override {
+    return full_msg.c_str();
+  }
+
+  const std::string msg;
+  const Location location;
+  const std::string full_msg;
+};
+
+/**
+ * Performs static elaboration of a program.
+ *
+ * The primary operation of static elaboration is deducing types and
+ * checking related constraints.
+ */
 class Typer {
  public:
+  Typer();
+
+  struct elaborate_topdecl_t {
+    managed_ptr<typing::Basis> B;
+    std::unique_ptr<TTopDecl> topdecl;
+  };
+
   /** Do typing analysis of a top-level declaration. */
-  std::pair<managed_ptr<typing::Basis>, std::unique_ptr<TTopDecl>> elaborate(
-      managed_ptr<typing::Basis> B, const TopDecl& topdec);
+  elaborate_topdecl_t elaborate(managed_ptr<typing::Basis> B,
+                                const TopDecl& topdecl);
+
+  struct elaborate_decl_t {
+    managed_ptr<typing::Env> env;
+    std::unique_ptr<TDecl> decl;
+  };
 
   /** Do typing analysis of a declaration. */
-  std::pair<managed_ptr<typing::Env>, std::unique_ptr<TDecl>> elaborate(
-      managed_ptr<typing::Context> C, const Decl& dec);
+  elaborate_decl_t elaborate(managed_ptr<typing::Context> C, const Decl& dec);
+
+  struct elaborate_decl_with_substitutions_t {
+    managed_ptr<typing::Env> env;
+    std::unique_ptr<TDecl> decl;
+    typing::Substitutions new_substitutions;
+  };
+
+  /** Do typing analysis of a declaration. */
+  elaborate_decl_with_substitutions_t elaborate(
+      managed_ptr<typing::Context> C, const Decl& dec,
+      typing::Substitutions& substitutions);
 
   /** Do typing analysis of an expression. */
   std::unique_ptr<TExpr> elaborate(managed_ptr<typing::Context> C,
                                    const Expr& exp);
 
- private:
-  class TopDeclElaborator : public TopDecl::Visitor {
-   public:
-    managed_ptr<typing::Basis> B;
-    std::unique_ptr<TTopDecl> typed;
-
-    TopDeclElaborator(Typer& typer, managed_ptr<typing::Basis> B);
-
-    DECLARE_TOPDECL_V_FUNCS;
-
-   private:
-    Typer& typer_;
+  struct elaborate_expr_with_substitutions_t {
+    std::unique_ptr<TExpr> expr;
+    typing::Substitutions new_substitutions;
   };
+
+  /**
+   * Do typing analysis of an expression.
+   *
+   * The given substitutions are applied to the expression's subtypes;
+   * newly deduced substitutions are added to substitutions as well as
+   * being returned separately in the result's new_substitutions
+   * field.
+   */
+  elaborate_expr_with_substitutions_t elaborate(
+      managed_ptr<typing::Context> C, const Expr& exp,
+      typing::Substitutions& substitutions, std::uint64_t maximum_type_name_id);
+
+  managed_ptr<typing::Type> elaborate_type_expr(managed_ptr<typing::Context> C,
+                                                const TypeExpr& ty);
+
+  managed_ptr<typing::Stamp> new_stamp();
+  const typing::BuiltinTypes& builtins() const;
+
+ private:
+  typing::StampGenerator stamp_generator_;
+  typing::BuiltinTypes builtins_;
 };
 
 }  // namespace emil
