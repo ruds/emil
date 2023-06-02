7dda2440c954e024c4b2eb2a2e2b5fd0feb5f562
Matt Rudary <matt@rudary.com>
2023-06-02 15:15:57 -0400

    Add TExpr type heirarchy.

--- include/emil/typed_ast.h
+++ include/emil/typed_ast.h
@@ -14,12 +14,22 @@
 
 #pragma once
 
+#include <cstdint>
 #include <memory>
+#include <stdexcept>
+#include <utility>
+#include <vector>
 
+#include "emil/bigint.h"  // IWYU pragma: keep
+#include "emil/cons.h"
+#include "emil/gc.h"
+#include "emil/string.h"
 #include "emil/token.h"
 #include "emil/types.h"
 
 namespace emil {
+
+class TDecl;
 
 /** A typed pattern. */
 class TPattern {
@@ -35,23 +45,336 @@ class TPattern {
   };
 
   virtual void accept(Visitor& visitor) const = 0;
+  virtual std::unique_ptr<TPattern> apply_substitutions(
+      typing::Substitutions substitutions) const = 0;
 };
+
+class TBigintLiteralExpr;
+class TIntLiteralExpr;
+class TFpLiteralExpr;
+class TStringLiteralExpr;
+class TCharLiteralExpr;
+class TFstringLiteralExpr;
+class TIdentifierExpr;
+class TRecRowExpr;
+class TRecordExpr;
+class TTupleExpr;
+class TSequencedExpr;
+class TListExpr;
+class TLetExpr;
+class TApplicationExpr;
+class TCaseExpr;
+class TFnExpr;
 
 /** A typed expression. */
 class TExpr {
  public:
+  /** The source location the expression starts. */
   const Location location;
+  /** The type the expression evaluates to. */
+  const typing::TypePtr type;
+  /**
+   * Whether the expression is non-expansive.
+   *
+   * When a val binding is elaborated, the type scheme(s) associated
+   * with the identifier(s) bound depend on whether the expression
+   * used in the binding's assignment is non-expansive. If it is, the
+   * scheme(s) generalize over the type variables introduced in the
+   * binding and are thus possibly polymorphic. If not, no
+   * generalization is performed and the type(s) are monomorphic. See
+   * sections 4.7 and 4.8 of the Definition of Standard ML (Revised).
+   */
+  const bool is_nonexpansive;
 
-  explicit TExpr(const Location& location);
+  TExpr(const Location& location, typing::TypePtr type, bool is_nonexpansive);
   virtual ~TExpr();
 
   class Visitor {
    public:
     virtual ~Visitor();
+
+    virtual void visit(const TBigintLiteralExpr& v) = 0;
+    virtual void visit(const TIntLiteralExpr& v) = 0;
+    virtual void visit(const TFpLiteralExpr& v) = 0;
+    virtual void visit(const TStringLiteralExpr& v) = 0;
+    virtual void visit(const TCharLiteralExpr& v) = 0;
+    virtual void visit(const TFstringLiteralExpr& v) = 0;
+    virtual void visit(const TIdentifierExpr& v) = 0;
+    virtual void visit(const TRecRowExpr& v) = 0;
+    virtual void visit(const TRecordExpr& v) = 0;
+    virtual void visit(const TTupleExpr& v) = 0;
+    virtual void visit(const TSequencedExpr& v) = 0;
+    virtual void visit(const TListExpr& v) = 0;
+    virtual void visit(const TLetExpr& v) = 0;
+    virtual void visit(const TApplicationExpr& v) = 0;
+    virtual void visit(const TCaseExpr& v) = 0;
+    virtual void visit(const TFnExpr& v) = 0;
   };
 
-  virtual typing::TypePtr type() const = 0;
   virtual void accept(Visitor& visitor) const = 0;
+
+  /** Apply substitutions for undetermined types in this expression
+   * and all its subexpressions. */
+  virtual std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const = 0;
+};
+
+/** A bigint literal. Nonexpansive. */
+class TBigintLiteralExpr : public TExpr {
+ public:
+  managed_ptr<bigint> value;
+
+  TBigintLiteralExpr(const Location& location, typing::TypePtr type,
+                     managed_ptr<bigint> value);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
+};
+
+/** An int literal. Nonexpansive. */
+class TIntLiteralExpr : public TExpr {
+ public:
+  std::int64_t value;
+
+  TIntLiteralExpr(const Location& location, typing::TypePtr type,
+                  std::int64_t value);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
+};
+
+/** A floating point literal. Nonexpansive. */
+class TFpLiteralExpr : public TExpr {
+ public:
+  double value;
+
+  TFpLiteralExpr(const Location& location, typing::TypePtr type, double value);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
+};
+
+/** A string literal. Nonexpansive. */
+class TStringLiteralExpr : public TExpr {
+ public:
+  StringPtr value;
+
+  TStringLiteralExpr(const Location& location, typing::TypePtr type,
+                     StringPtr value);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
+};
+
+/** A character literal. Nonexpansive. */
+class TCharLiteralExpr : public TExpr {
+ public:
+  char32_t value;
+
+  TCharLiteralExpr(const Location& location, typing::TypePtr type,
+                   char32_t value);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
+};
+
+/** A formatted string literal. Nonexpansive iff all substitutions are
+ * nonexpanisive. */
+class TFstringLiteralExpr : public TExpr {
+ public:
+  collections::ConsPtr<ManagedString> segments;
+  std::vector<std::unique_ptr<TExpr>> substitutions;
+
+  TFstringLiteralExpr(const Location& location, typing::TypePtr type,
+                      collections::ConsPtr<ManagedString> segments,
+                      std::vector<std::unique_ptr<TExpr>> substitutions);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
+};
+
+/** An identifier expression. Nonexpansive. */
+class TIdentifierExpr : public TExpr {
+ public:
+  collections::ConsPtr<ManagedString> qualifiers;
+  StringPtr canonical_identifier;
+
+  TIdentifierExpr(const Location& location, typing::TypePtr type,
+                  collections::ConsPtr<ManagedString> qualifiers,
+                  StringPtr canonical_identifier);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
+};
+
+/**
+ * A record row expression. Nonexpansive iff its value is nonexpansive.
+ *
+ * This is sort of a quasi-expression type; it's almost never correct
+ * for visitors to visit this through `visit`; instead record rows
+ * should be processed as part of the enclosing record.
+ */
+class TRecRowExpr : public TExpr {
+ public:
+  StringPtr label;
+  std::unique_ptr<TExpr> value;
+
+  TRecRowExpr(const Location& location, typing::TypePtr type, StringPtr label,
+              std::unique_ptr<TExpr> value);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+
+  std::unique_ptr<TRecRowExpr> apply_substitutions_as_rec_row(
+      typing::Substitutions substitutions) const;
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions) const override {
+    throw std::logic_error("unreachable");
+  }
+};
+
+/** A record expression. Nonexpansive iff all its rows are nonexpansive. */
+class TRecordExpr : public TExpr {
+ public:
+  std::vector<std::unique_ptr<TRecRowExpr>> rows;
+
+  TRecordExpr(const Location& location, typing::TypePtr type,
+              std::vector<std::unique_ptr<TRecRowExpr>> rows);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
+};
+
+/** A tuple expression. Nonexpansive iff all its elements are nonexpansive. */
+class TTupleExpr : public TExpr {
+ public:
+  std::vector<std::unique_ptr<TExpr>> exprs;
+
+  TTupleExpr(const Location& location, typing::TypePtr type,
+             std::vector<std::unique_ptr<TExpr>> exprs);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
+};
+
+/** A sequenced expression. Expansive. */
+class TSequencedExpr : public TExpr {
+ public:
+  std::vector<std::unique_ptr<TExpr>> exprs;
+
+  TSequencedExpr(const Location& location, typing::TypePtr type,
+                 std::vector<std::unique_ptr<TExpr>> exprs);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
+};
+
+/** A list expression. Nonexpansive iff all its elements are nonexpansive. */
+class TListExpr : public TExpr {
+ public:
+  std::vector<std::unique_ptr<TExpr>> exprs;
+
+  TListExpr(const Location& location, typing::TypePtr type,
+            std::vector<std::unique_ptr<TExpr>> exprs);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
+};
+
+/** A let expression. Expansive. */
+class TLetExpr : public TExpr {
+ public:
+  std::vector<std::unique_ptr<TDecl>> decls;
+  std::vector<std::unique_ptr<TExpr>> exprs;
+
+  TLetExpr(const Location& location, typing::TypePtr type,
+           std::vector<std::unique_ptr<TDecl>> decls,
+           std::vector<std::unique_ptr<TExpr>> exprs);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
+};
+
+/**
+ * A function application expression. Expansiveness depends on context.
+ *
+ * Function applications can be "true" function applications, or else
+ * application of a type constructor or exception constructor. True
+ * function applications are expansive. Applyication of a type
+ * constructor or exception constructor to a nonexpansive expression
+ * is nonexpansive.
+ *
+ * This information is available in the elaboration context and should
+ * be computed by the elaborator when constructing this object.
+ */
+class TApplicationExpr : public TExpr {
+ public:
+  std::vector<std::unique_ptr<TExpr>> exprs;
+
+  TApplicationExpr(const Location& location, typing::TypePtr type,
+                   bool is_nonexpansive,
+                   std::vector<std::unique_ptr<TExpr>> exprs);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
+};
+
+/** A case expression. Expansive. */
+class TCaseExpr : public TExpr {
+ public:
+  std::unique_ptr<TExpr> expr;
+  std::vector<std::pair<std::unique_ptr<TPattern>, std::unique_ptr<TExpr>>>
+      cases;
+
+  TCaseExpr(
+      const Location& location, typing::TypePtr type,
+      std::unique_ptr<TExpr> expr,
+      std::vector<std::pair<std::unique_ptr<TPattern>, std::unique_ptr<TExpr>>>
+          cases);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
+};
+
+/** A function literal expression. Nonexpansive. */
+class TFnExpr : public TExpr {
+ public:
+  std::vector<std::pair<std::unique_ptr<TPattern>, std::unique_ptr<TExpr>>>
+      cases;
+
+  TFnExpr(
+      const Location& location, typing::TypePtr type,
+      std::vector<std::pair<std::unique_ptr<TPattern>, std::unique_ptr<TExpr>>>
+          cases);
+
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
+
+  std::unique_ptr<TExpr> apply_substitutions(
+      typing::Substitutions substitutions) const override;
 };
 
 /** A typed declaration. */
@@ -68,6 +391,8 @@ class TDecl {
   };
 
   virtual void accept(Visitor& visitor) const = 0;
+  virtual std::unique_ptr<TDecl> apply_substitutions(
+      typing::Substitutions substitutions) const = 0;
 };
 
 class TEmptyTopDecl;
@@ -100,14 +425,14 @@ class TEmptyTopDecl : public TTopDecl {
  public:
   explicit TEmptyTopDecl(const Location& location);
 
-  void accept(Visitor& visitor) const override;
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
 };
 
 class TEndOfFileTopDecl : public TTopDecl {
  public:
   explicit TEndOfFileTopDecl(const Location& location);
 
-  void accept(Visitor& visitor) const override;
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
 };
 
 class TExprTopDecl : public TTopDecl {
@@ -116,7 +441,7 @@ class TExprTopDecl : public TTopDecl {
 
   TExprTopDecl(const Location& location, std::unique_ptr<TExpr> expr);
 
-  void accept(Visitor& visitor) const override;
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
 };
 
 class TDeclTopDecl : public TTopDecl {
@@ -125,7 +450,7 @@ class TDeclTopDecl : public TTopDecl {
 
   TDeclTopDecl(const Location& location, std::unique_ptr<TDecl> decl);
 
-  void accept(Visitor& visitor) const override;
+  void accept(Visitor& visitor) const override { visitor.visit(*this); }
 };
 
 }  // namespace emil
--- src/typed_ast.cc
+++ src/typed_ast.cc
@@ -14,6 +14,11 @@
 
 #include "emil/typed_ast.h"
 
+#include <algorithm>
+#include <iterator>
+
+#include "emil/types.h"
+
 namespace emil {
 
 TPattern::~TPattern() = default;
@@ -21,6 +26,237 @@ TPattern::Visitor::~Visitor() = default;
 
 TExpr::~TExpr() = default;
 TExpr::Visitor::~Visitor() = default;
+
+namespace {
+
+template <typename T>
+bool all_nonexp(const T& types) {
+  return std::all_of(types.begin(), types.end(),
+                     [](const auto& t) { return t->is_nonexpansive; });
+}
+
+template <typename T>
+std::vector<std::unique_ptr<T>> apply_substitutions_to_list(
+    const std::vector<std::unique_ptr<T>>& exprs,
+    typing::Substitutions substitutions) {
+  std::vector<std::unique_ptr<T>> new_exprs;
+  new_exprs.reserve(exprs.size());
+  std::transform(
+      exprs.begin(), exprs.end(), back_inserter(new_exprs),
+      [&](const auto& e) { return e->apply_substitutions(substitutions); });
+  return new_exprs;
+}
+
+template <typename T, typename U>
+std::vector<std::pair<std::unique_ptr<T>, std::unique_ptr<U>>>
+apply_substitutions_to_pairs(
+    const std::vector<std::pair<std::unique_ptr<T>, std::unique_ptr<U>>>& pairs,
+    typing::Substitutions substitutions) {
+  std::vector<std::pair<std::unique_ptr<T>, std::unique_ptr<U>>> new_pairs;
+  new_pairs.reserve(pairs.size());
+  std::transform(
+      pairs.begin(), pairs.end(), back_inserter(new_pairs), [&](const auto& p) {
+        return std::make_pair(p.first->apply_substitutions(substitutions),
+                              p.second->apply_substitutions(substitutions));
+      });
+  return new_pairs;
+}
+
+}  // namespace
+
+TBigintLiteralExpr::TBigintLiteralExpr(const Location& location,
+                                       typing::TypePtr type,
+                                       managed_ptr<bigint> value)
+    : TExpr(location, type, true), value(value) {}
+
+std::unique_ptr<TExpr> TBigintLiteralExpr::apply_substitutions(
+    typing::Substitutions) const {
+  return std::make_unique<TBigintLiteralExpr>(location, type, value);
+}
+
+TIntLiteralExpr::TIntLiteralExpr(const Location& location, typing::TypePtr type,
+                                 std::int64_t value)
+    : TExpr(location, type, true), value(value) {}
+
+std::unique_ptr<TExpr> TIntLiteralExpr::apply_substitutions(
+    typing::Substitutions) const {
+  return std::make_unique<TIntLiteralExpr>(location, type, value);
+}
+
+TFpLiteralExpr::TFpLiteralExpr(const Location& location, typing::TypePtr type,
+                               double value)
+    : TExpr(location, type, true), value(value) {}
+
+std::unique_ptr<TExpr> TFpLiteralExpr::apply_substitutions(
+    typing::Substitutions) const {
+  return std::make_unique<TFpLiteralExpr>(location, type, value);
+}
+
+TStringLiteralExpr::TStringLiteralExpr(const Location& location,
+                                       typing::TypePtr type, StringPtr value)
+    : TExpr(location, type, true), value(value) {}
+
+std::unique_ptr<TExpr> TStringLiteralExpr::apply_substitutions(
+    typing::Substitutions) const {
+  return std::make_unique<TStringLiteralExpr>(location, type, value);
+}
+
+TCharLiteralExpr::TCharLiteralExpr(const Location& location,
+                                   typing::TypePtr type, char32_t value)
+    : TExpr(location, type, true), value(value) {}
+
+std::unique_ptr<TExpr> TCharLiteralExpr::apply_substitutions(
+    typing::Substitutions) const {
+  return std::make_unique<TCharLiteralExpr>(location, type, value);
+}
+
+TFstringLiteralExpr::TFstringLiteralExpr(
+    const Location& location, typing::TypePtr type,
+    collections::ConsPtr<ManagedString> segments,
+    std::vector<std::unique_ptr<TExpr>> substitutions)
+    : TExpr(location, type, all_nonexp(substitutions)),
+      segments(segments),
+      substitutions(std::move(substitutions)) {}
+
+std::unique_ptr<TExpr> TFstringLiteralExpr::apply_substitutions(
+    typing::Substitutions var_substitutions) const {
+  return std::make_unique<TFstringLiteralExpr>(
+      location, type, segments,
+      apply_substitutions_to_list(substitutions, var_substitutions));
+}
+
+TIdentifierExpr::TIdentifierExpr(const Location& location, typing::TypePtr type,
+                                 collections::ConsPtr<ManagedString> qualifiers,
+                                 StringPtr canonical_identifier)
+    : TExpr(location, type, true),
+      qualifiers(qualifiers),
+      canonical_identifier(canonical_identifier) {}
+
+std::unique_ptr<TExpr> TIdentifierExpr::apply_substitutions(
+    typing::Substitutions substitutions) const {
+  return std::make_unique<TIdentifierExpr>(
+      location, typing::apply_substitutions(type, substitutions), qualifiers,
+      canonical_identifier);
+}
+
+TRecRowExpr::TRecRowExpr(const Location& location, typing::TypePtr type,
+                         StringPtr label, std::unique_ptr<TExpr> value)
+    : TExpr(location, type, value->is_nonexpansive),
+      label(label),
+      value(std::move(value)) {}
+
+std::unique_ptr<TRecRowExpr> TRecRowExpr::apply_substitutions_as_rec_row(
+    typing::Substitutions substitutions) const {
+  return std::make_unique<TRecRowExpr>(
+      location, typing::apply_substitutions(type, substitutions), label,
+      value->apply_substitutions(substitutions));
+}
+
+TRecordExpr::TRecordExpr(const Location& location, typing::TypePtr type,
+                         std::vector<std::unique_ptr<TRecRowExpr>> rows)
+    : TExpr(location, type, all_nonexp(rows)), rows(std::move(rows)) {}
+
+std::unique_ptr<TExpr> TRecordExpr::apply_substitutions(
+    typing::Substitutions substitutions) const {
+  std::vector<std::unique_ptr<TRecRowExpr>> new_rows;
+  new_rows.reserve(rows.size());
+  std::transform(rows.begin(), rows.end(), back_inserter(new_rows),
+                 [&](const auto& r) {
+                   return r->apply_substitutions_as_rec_row(substitutions);
+                 });
+  return std::make_unique<TRecordExpr>(
+      location, typing::apply_substitutions(type, substitutions),
+      std::move(new_rows));
+}
+
+TTupleExpr::TTupleExpr(const Location& location, typing::TypePtr type,
+                       std::vector<std::unique_ptr<TExpr>> exprs)
+    : TExpr(location, type, all_nonexp(exprs)), exprs(std::move(exprs)) {}
+
+std::unique_ptr<TExpr> TTupleExpr::apply_substitutions(
+    typing::Substitutions substitutions) const {
+  return std::make_unique<TTupleExpr>(
+      location, typing::apply_substitutions(type, substitutions),
+      apply_substitutions_to_list(exprs, substitutions));
+}
+
+TSequencedExpr::TSequencedExpr(const Location& location, typing::TypePtr type,
+                               std::vector<std::unique_ptr<TExpr>> exprs)
+    : TExpr(location, type, false), exprs(std::move(exprs)) {}
+
+std::unique_ptr<TExpr> TSequencedExpr::apply_substitutions(
+    typing::Substitutions substitutions) const {
+  return std::make_unique<TSequencedExpr>(
+      location, typing::apply_substitutions(type, substitutions),
+      apply_substitutions_to_list(exprs, substitutions));
+}
+
+TListExpr::TListExpr(const Location& location, typing::TypePtr type,
+                     std::vector<std::unique_ptr<TExpr>> exprs)
+    : TExpr(location, type, all_nonexp(exprs)), exprs(std::move(exprs)) {}
+
+std::unique_ptr<TExpr> TListExpr::apply_substitutions(
+    typing::Substitutions substitutions) const {
+  return std::make_unique<TListExpr>(
+      location, typing::apply_substitutions(type, substitutions),
+      apply_substitutions_to_list(exprs, substitutions));
+}
+
+TLetExpr::TLetExpr(const Location& location, typing::TypePtr type,
+                   std::vector<std::unique_ptr<TDecl>> decls,
+                   std::vector<std::unique_ptr<TExpr>> exprs)
+    : TExpr(location, type, false),
+      decls(std::move(decls)),
+      exprs(std::move(exprs)) {}
+
+std::unique_ptr<TExpr> TLetExpr::apply_substitutions(
+    typing::Substitutions substitutions) const {
+  return std::make_unique<TLetExpr>(
+      location, typing::apply_substitutions(type, substitutions),
+      apply_substitutions_to_list(decls, substitutions),
+      apply_substitutions_to_list(exprs, substitutions));
+}
+
+TApplicationExpr::TApplicationExpr(const Location& location,
+                                   typing::TypePtr type, bool is_nonexpansive,
+                                   std::vector<std::unique_ptr<TExpr>> exprs)
+    : TExpr(location, type, is_nonexpansive), exprs(std::move(exprs)) {}
+
+std::unique_ptr<TExpr> TApplicationExpr::apply_substitutions(
+    typing::Substitutions substitutions) const {
+  return std::make_unique<TApplicationExpr>(
+      location, typing::apply_substitutions(type, substitutions),
+      is_nonexpansive, apply_substitutions_to_list(exprs, substitutions));
+}
+
+TCaseExpr::TCaseExpr(
+    const Location& location, typing::TypePtr type, std::unique_ptr<TExpr> expr,
+    std::vector<std::pair<std::unique_ptr<TPattern>, std::unique_ptr<TExpr>>>
+        cases)
+    : TExpr(location, type, false),
+      expr(std::move(expr)),
+      cases(std::move(cases)) {}
+
+std::unique_ptr<TExpr> TCaseExpr::apply_substitutions(
+    typing::Substitutions substitutions) const {
+  return std::make_unique<TCaseExpr>(
+      location, typing::apply_substitutions(type, substitutions),
+      expr->apply_substitutions(substitutions),
+      apply_substitutions_to_pairs(cases, substitutions));
+}
+
+TFnExpr::TFnExpr(
+    const Location& location, typing::TypePtr type,
+    std::vector<std::pair<std::unique_ptr<TPattern>, std::unique_ptr<TExpr>>>
+        cases)
+    : TExpr(location, type, true), cases(std::move(cases)) {}
+
+std::unique_ptr<TExpr> TFnExpr::apply_substitutions(
+    typing::Substitutions substitutions) const {
+  return std::make_unique<TFnExpr>(
+      location, typing::apply_substitutions(type, substitutions),
+      apply_substitutions_to_pairs(cases, substitutions));
+}
 
 TDecl::~TDecl() = default;
 TDecl::Visitor::~Visitor() = default;
